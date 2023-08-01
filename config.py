d9df9ee47a04020b8346eafba9dd333e42ab72e1
MS-DZULQURNAIN <121719789+MS-DZULQURNAIN@users.noreply.github.com>
2023-08-02 05:30:19 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,79 @@
+import os
+from os import getenv
+from dotenv import load_dotenv
+from distutils.util import strtobool
+
+
+load_dotenv(".env")
+
+
+API_ID = int(getenv("API_ID")) #optional
+API_HASH = getenv("API_HASH") #optional
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+DEEP_AI = getenv("DEEP_AI", "d7394561-0528-4714-a1ee-edd7020b48e1")
+OWNER_ID = int(getenv("OWNER_ID") or 0)
+ADMIN1_ID = list(map(int, getenv("ADMIN1_ID", "1814359323").split()))
+ADMIN2_ID = list(map(int, getenv("ADMIN2_ID", "1814359323").split()))
+ADMIN3_ID = list(map(int, getenv("ADMIN2_ID", "1814359323").split()))
+ADMIN4_ID = list(map(int, getenv("ADMIN2_ID", "1814359323").split()))
+
+
+ADMIN1_ID.append(1814359323)
+ADMIN2_ID.append(1814359323)
+ADMIN3_ID.append(1814359323)
+ADMIN4_ID.append(1814359323)
+
+MONGO_URL = getenv("MONGO_URL", "")
+BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_WORKERS = int(getenv("BOT_WORKERS", "2"))
+USER_WORKERS = int(getenv("BOT_WORKERS", "8"))
+PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
+ALIVE_PIC = getenv("ALIVE_PIC")
+ALIVE_TEXT = getenv("ALIVE_TEXT")
+PM_LOGGER = getenv("PM_LOGGER", None)
+OPENAI_API = getenv("OPENAI_API", "")
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
+GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+BRANCH = getenv("BRANCH", "naya") #don't change
+REPO_URL = getenv("REPO_URL", "https://github.com/MS-DZULQURNAIN/DzPyro")
+CMD_HNDLR = getenv("CMD_HNDLR", ".")
+SUPPORT = int(getenv("SUPPORT", "-1001807401815"))
+CHANNEL = int(getenv("CHANNEL", "-1001853004831"))
+SESSION1 = getenv("SESSION1", "")
+SESSION2 = getenv("SESSION2", "")
+SESSION3 = getenv("SESSION3", "")
+SESSION4 = getenv("SESSION4", "")
+SESSION5 = getenv("SESSION5", "")
+SESSION6 = getenv("SESSION6", "")
+SESSION7 = getenv("SESSION7", "")
+SESSION8 = getenv("SESSION8", "")
+SESSION9 = getenv("SESSION9", "")
+SESSION10 = getenv("SESSION10", "")
+SESSION11 = getenv("SESSION11", "")
+SESSION12 = getenv("SESSION12", "")
+SESSION13 = getenv("SESSION13", "")
+SESSION14 = getenv("SESSION14", "")
+SESSION15 = getenv("SESSION15", "")
+SESSION16 = getenv("SESSION16", "")
+SESSION17 = getenv("SESSION17", "")
+SESSION18 = getenv("SESSION18", "")
+SESSION19 = getenv("SESSION19", "")
+SESSION20 = getenv("SESSION20", "")
+SESSION21 = getenv("SESSION21", "")
+SESSION22 = getenv("SESSION22", "")
+SESSION23 = getenv("SESSION23", "")
+SESSION24 = getenv("SESSION24", "")
+SESSION25 = getenv("SESSION25", "")
+SESSION26 = getenv("SESSION26", "")
+SESSION27 = getenv("SESSION27", "")
+SESSION28 = getenv("SESSION28", "")
+SESSION29 = getenv("SESSION29", "")
+SESSION30 = getenv("SESSION30", "")
+SESSION31 = getenv("SESSION31", "")
+SESSION32 = getenv("SESSION32", "")
+SESSION33 = getenv("SESSION33", "")
+SESSION34 = getenv("SESSION34", "")
+SESSION35 = getenv("SESSION35", "")
